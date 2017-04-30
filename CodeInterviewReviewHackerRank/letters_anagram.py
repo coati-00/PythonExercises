f8820899ed709d6e987cb440cabe59de58659a6d
Cassandra Dunlop <cdulop@columbia.com>
2017-04-30 16:43:08 -0400

    update

--- CodeInterviewReviewHackerRank/letters_anagram.py
+++ CodeInterviewReviewHackerRank/letters_anagram.py
@@ -1,5 +1,9 @@
-'''A common question on interviews is to write a function
-to tell if two strings are anagrams are one another'''
+'''This is a second variation of the anagram question, except instead of
+asking you to write a function that tells you if two strings are anagrams
+they ask you to tell you how many letters you need to remove to turn the
+strings into anagrams... so for example abcd dcfg you would need to remove
+a and b from the first string and f and g from the second.'''
+
 
 def get_letter_count(somestring):
     '''get individual letters in each string'''
@@ -11,14 +15,15 @@ def get_letter_count(somestring):
             temp_alph[letter] = 1
     return temp_alph
 
-def are_anagrams(str1, str2):
+def compare_letters_for_anagrams(str1, str2):
     '''given two strings determine if they are anagrams'''
     temp_alph1 = get_letter_count(str1)
     temp_alph2 = get_letter_count(str2)
     
-    if len(temp_alph1.keys) != len(temp_alph2.keys):
+    if len(temp_alph1.keys()) != len(temp_alph2.keys()):
         print "not anagams"
         return False
+
     for each in temp_alph1.keys():
         if each in temp_alph2.keys():
             if temp_alph1[each] != temp_alph2[each]:
@@ -37,13 +42,14 @@ def are_anagrams(str1, str2):
 
 teststr1 = "abcd"
 teststr2 = "cdef"
-teststr3 = "abcd"
-teststr4 = "abcd"
+teststr3 = "abcdef"
+teststr4 = "abcdefghi"
 
 
-
-are_anagrams(teststr1, teststr2)
-are_anagrams(teststr3, teststr4)
+compare_letters_for_anagrams(teststr1, teststr2)
+compare_letters_for_anagrams(teststr1, teststr1)
+compare_letters_for_anagrams(teststr3, teststr1)
+compare_letters_for_anagrams(teststr1, teststr4)
 
 
 
@@ -51,4 +57,3 @@ are_anagrams(teststr3, teststr4)
 
 #if __name__ == '__main__':
     
-
