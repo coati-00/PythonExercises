40f9d12d3872d174ebbe38fd8851780f0b3290c3
coati-00 <coati.00@gmail.com>
2017-08-16 16:58:27 -0400

    update

--- HackerRankExercises/birthday_cake_candles.py
+++ HackerRankExercises/birthday_cake_candles.py
@@ -5,31 +5,22 @@ import sys
 def birthdayCakeCandles(n, ar):
     # get tallest candle, if there are several tall candles - count them
     tallest = ar[0]
-    count = 1
+    count = 0
 
     # going to do this the long way, and improve later
-    for index in range(0,len(ar),+1):
-        '''start by looking for tallest candle'''
-        if ar[index + 1] > ar[index]:
-            tallest = ar[index + 1]
-    print tallest
+    for number in ar:
+        if number > tallest:
+            tallest = number
+
     for each in ar:
         if each == tallest:
             count = count + 1
-    print count
-    # print count
+
     return count
-        
-        
-        
+
+
+
 n = int(raw_input().strip())
 ar = map(int, raw_input().strip().split(' '))
 result = birthdayCakeCandles(n, ar)
 print(result)
-
-
-'''
-Sample Input:
-4
-3 2 1 3
-'''
